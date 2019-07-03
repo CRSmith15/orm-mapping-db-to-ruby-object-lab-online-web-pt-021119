97e8b0bc53c451982e193bfe277546d8c26511c6
Chance Smith <chancesmith97@hotmail.com>
2019-07-03 01:09:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,9 @@ class Student
     LIMIT 
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.find_by_name(name)
