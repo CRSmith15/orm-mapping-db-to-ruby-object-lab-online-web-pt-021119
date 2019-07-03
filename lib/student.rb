faaf106ebec6a8bfe2bc358e2610698842889edb
Chance Smith <chancesmith97@hotmail.com>
2019-07-03 01:20:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,9 @@ class Student
     WHERE grade = ?
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql,x).map do |row|
+      self.new_from_db(row)
+    end
   end   
 
   def self.find_by_name(name)
