5ead1141f4c852c36e65703d184f65aea8c6f827
Chance Smith <chancesmith97@hotmail.com>
2019-07-03 00:57:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,6 +43,15 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT name 
+    FROM students 
+    WHERE grade = 10 
+    LIMIT x 
+    SQL
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
